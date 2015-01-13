8117afa72d4f0ce59d32211febe0a14a24c51433
vidalalcala <vidal.alcala@gmail.com>
2015-01-12 18:57:21 -0600

    finish quadratic test, need unit test

--- affine-sopt-quadratic.lua
+++ affine-sopt-quadratic.lua
@@ -17,7 +17,8 @@ require 'randomkit'
 local opt = lapp([[
    -t,--threads  (default 7)         number of threads
    -p,--nbPar    (default 4) 	     number of parameters
-   -N,--nbIter   (default 10)      number of iterations
+   -N,--nbIter   (default 20)        number of iterations
+   -g,--gamma    (default 0.6)       power lag
 ]])
 
 -- threads
@@ -25,29 +26,30 @@ local opt = lapp([[
 -- print('<torch> set nb of threads to ' .. torch.getnumthreads())
 
 -- Initial seed
-torch.manualSeed(123)
+torch.manualSeed(234)
 
 -- Create synthetic data
 local p = opt.nbPar
-local n = p + 1      --required for full rank initialization
+local nInit = p + 1      --required for full rank initialization
 local N = opt.nbIter
+local gamma = opt.gamma
 
 local H = torch.rand(p, p)
-H = ( H:t() + H )
-H:mul(0.5)
+H = H:t() * H 
 local Sigma = torch.rand(p, p)
-Sigma = ( Sigma:t() + Sigma )
-Sigma:mul(0.5)
+Sigma = Sigma:t() * Sigma
 local alphaOptimal = torch.rand(1, p)
 
 -- print problem setup
 print('H : ')
 print(H)
+print(torch.symeig(H))
 local HInverse = torch.inverse(H)
 print('HInverse : ')
 print(HInverse)
 print('Sigma : ')
 print(Sigma)
+print(torch.symeig(Sigma))
 print('alphaOptimal : ')
 print(alphaOptimal)
 
@@ -59,26 +61,33 @@ function  stochasticGradientQuadratic(alpha)
   return estimator
 end
 
--- generate first n = p + 1 states randomly
-local X = torch.rand(n, p)
-
+-- generate first nInit = p + 1 states randomly
+local X = torch.rand(nInit, p)
+print('X : ')
 print(X)
 
--- Initialize the estimators
-local Y = torch.Tensor(n, p)
-for i = 1, n, 1 do
+-- generate first nInit = p + 1 gradient observations
+local Y = torch.Tensor(nInit, p)
+for i = 1, nInit, 1 do
   local alpha = torch.Tensor(1, p)
   alpha:copy(X[{i,{}}])
   local gradAlpha = stochasticGradientQuadratic(alpha)
   Y[{i,{}}] = gradAlpha
 end
 
+print('Y : ')
+print(Y)
+
+-- Use first n = p +1 observations to construct initial estimators
+local n = nInit
 local X = torch.cat(X , torch.ones(n, 1), 2)
 local P = torch.inverse( X:t() * X )
 local B = P * (X:t() * Y)
 local G = torch.inverse(B[{{1,p},{}}])
+local HEstimator = G + G:t()
+HEstimator:mul(0.5)
 
--- 
+-- Online addition of observation
 function addObservation(x, y)
   n = n + 1
   x = torch.cat(x, torch.ones(1,1), 2)
@@ -100,8 +109,45 @@ function addObservation(x, y)
   GAdd = (G * u * v:t() * G)
   GAdd:mul(beta[1][1])
   G = G + GAdd
+  HEstimator = G + G:t()
+  HEstimator:mul(0.5)
 end
 
---
+
+-- Initial estimator
+print('initial H inverse estimator : ')
+print(HEstimator)
+-- Initial estimator
+print('initial B estimator : ')
+print(B)
+
+-- Perform Robbins-Monro with OLS
+local alpha = torch.rand(1, p)
+local alphaNew = torch.Tensor(1, p)
+local gradAlpha = stochasticGradientQuadratic(alpha)
+local gradAlphaNew = torch.rand(1, p)
+
+for n = nInit + 1, N, 1 do
+  alphaNew = gradAlpha*HEstimator
+  alphaNew:mul(-1.0/(n^gamma))
+  alphaNew = alphaNew + alpha
+  gradAlphaNew = stochasticGradientQuadratic(alphaNew)
+  addObservation(alpha, gradAlpha)
+  alpha = alphaNew:clone()
+  gradAlpha = gradAlphaNew:clone()
+end
+
+print('alpha estimator : ')
+print(alphaNew)
+print('H inverse estimator : ')
+print(HEstimator)
+print('gamma : ')
+print(gamma)
+print('number of gradient samples : ')
+print(n)
+
+
+
+
 
 
